66c3f7ae3f31ff251dfa3759e38e27d76f0f3f46
torkelhi <torkelhi@uia.no>
2022-10-04 15:49:30 +0200

    BrukerController lagt til

---
+++ PDSA_System/Server/Controllers/BrukerController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PDSA_System.Server.Models;
+using Dapper;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PDSA_System.Server.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+
+    public class BrukerController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public BrukerController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        /**
+         * Henter alle brukerene fra databasen.
+         * Returnerer statuskode 200 dersom det ikke oppstår feil.
+         */
+        [HttpGet]
+        public async Task<ActionResult<List<Bruker>>> GetAllBrukere()
+        {
+
+            var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+            using var conn = new DbHelper(connString).Connection;
+
+            var brukere = await conn.QueryAsync<Bruker>("SELECT * FROM Bruker");
+
+            return Ok(brukere);
+        }
+
+
+        /**
+         * Henter en spesifikk bruker iforhold til hvilken route man er på.
+         * URL --> NordicDoor/Bruker/1 vil hente ut bruker med brukerId 1.
+         * Returnerer statuskode 200 dersom det ikke oppstår feil.
+        */
+
+        [HttpGet("/api/[controller]/{brukerId}")]
+        public async Task<ActionResult<List<Bruker>>> GetBruker(int brukerId)
+        {
+            var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+            using var conn = new DbHelper(connString).Connection;
+
+            var brukere = await conn.QueryAsync<Bruker>("SELECT * FROM Bruker WHERE BrukerId = @id",
+                new { id = brukerId });
+
+            return Ok(brukere);
+        }
+
+
+        /*
+            Create for en ny Bruker.
+            Fungerer ikke akkurat nå siden PassordHash har sikkerthetsfunksjoner jeg ikke kjenner til.
+         */
+        [HttpPost]
+        public async Task<ActionResult<List<Bruker>>> CreateBruker(Bruker bruker)
+        {
+            var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+            using var conn = new DbHelper(connString).Connection;
+
+            await conn.ExecuteAsync(
+                "INSERT INTO Bruker(ForNavn, EtterNavn, Email, Rolle, PassordHash) values (@ForNavn, @EtterNavn, @Email, @Rolle, @PassordHash))", bruker);
+
+            return Ok(await GetBruker(bruker.BrukerId));
+        }
+
+        [HttpPut]
+
+        public async Task<ActionResult<List<Bruker>>> UpdateBruker(Bruker bruker)
+        {
+            var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+            using var conn = new DbHelper(connString).Connection;
+
+            await conn.ExecuteAsync(
+                "UPDATE Bruker SET ForNavn = @ForNavn, EtterNavn = @EtterNavn, Email = @Email, Rolle = @Rolle, PassordHash = @PassordHash", bruker);
+
+            return Ok(await GetBruker(bruker.BrukerId));
+        }
+
+
+        [HttpDelete]
+
+        public async Task<ActionResult<List<Bruker>>> DeleteBruker(Bruker bruker)
+        {
+            var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+            using var conn = new DbHelper(connString).Connection;
+
+            await conn.ExecuteAsync(
+                "DELETE FROM Bruker WHERE BrukerId = @BrukerId", bruker);
+
+            return Ok(await GetAllBrukere());
+        }
+    }
+}
+
+
+
