c27d6803219b282b21201e62d23a37d0a3d2a597
EirikBHA <89193729+EirikBHA@users.noreply.github.com>
2022-10-18 11:22:23 +0200

    Implementert CRUD-funksjonalitet for ForslagController. Update funker ikke helt enda, og man må fjerne bilde string i swagger for å teste legg til forslag.

--- PDSA_System/Server/Controllers/ForslagController.cs
+++ PDSA_System/Server/Controllers/ForslagController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using PDSA_System.Server.Models;
 using Dapper;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Pkcs;
 
 namespace PDSA_System.Server.Controllers;
 
@@ -35,18 +37,89 @@ public class ForslagController : Controller
     }
 
     /**
-    * Funksjon for å opprette forslag
-    * Returnerer statuskode 200 dersom det ikke oppstår feil.
-    * Fjernet bilde da det ikke fungerte å legge til pga kluss med datatype
+     * Funksjon for å hente ut spesifikke forslag
+     * Returnerer statuskode 200 dersom det ikke oppstår feil.
      */
-    [HttpPost("/api/[controller]/createforslag/")]
-    public async Task<ActionResult<List<Forslag>>> CreateForslag(Forslag forslag)
+    [HttpGet("/api/[controller]/{forslagId}")]
+    public async Task<ActionResult<List<Forslag>>> GetForslag(int forslagId)
     {
         var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
         using var conn = new DbHelper(connString).Connection;
 
+        var forslag = await conn.QueryAsync<Forslag>("SELECT * FROM Forslag WHERE ForslagsId = @id",
+            new { id = forslagId });
+
+        return Ok(forslag);
+    }
+
+    /**
+    * Funksjon for å opprette forslag
+    * Returnerer statuskode 200 dersom det ikke oppstår feil.
+    * Fjernet bilde da det ikke fungerte å legge til pga kluss med datatype
+    * Hvis den skal testes i swagger fjern bilde stringen og kjør
+     */
+    [HttpPost("/api/[controller]/createforslag/")]
+    public async Task<ActionResult<List<Forslag>>> CreateForslag(Forslag forslag)
+    {
+        //byte[] bilde = GetBilde("C:/Bjønn.jpeg");
+
+        var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        using var conn = new DbHelper(connString).Connection;
+
         await conn.ExecuteAsync("INSERT INTO Forslag (ForfatterId, TeamId, Emne, Beskrivelse, Kategori) VALUES (@ForfatterId, @TeamId, @Emne, @Beskrivelse, @Kategori)", forslag);
+        
+        return Ok(await GetAllForslag());
+    }
+
+    /**
+     * Funksjon for å oppdatere forslag
+     * Returnerer statuskode 200 dersom det ikke oppstår feil.
+     * Funker ikke helt enda.
+     */
+    [HttpPut("/api/[controller]/updateforslag/{forslagId}")]
+    public async Task<ActionResult<List<Forslag>>> UpdateForslag(int forslagId, Forslag forslag)
+    {
+        var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        using var conn = new DbHelper(connString).Connection;
+
+        await conn.ExecuteAsync(
+            "UPDATE Forslag SET ForfatterId = @ForfatterId, TeamId = @TeamId, Emne = @Emne, Beskrivelse = @Beskrivelse, Bilde = @Bilde, Kategori = @Kategori WHERE ForslagsId = @ForslagsId",
+            forslag);
 
         return Ok(await GetAllForslag());
     }
+
+    /**
+     * Funksjon for å slette forslag utifra forslagId
+     * Returnerer statuskode 200 dersom det ikke oppstår feil
+     */
+    [HttpDelete("/api/[controller]/deleteforslag/{forslagId}")]
+    public async Task<ActionResult<List<Forslag>>> DeleteForslag(int forslagId)
+    {
+        var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        using var conn = new DbHelper(connString).Connection;
+
+        await conn.ExecuteAsync("DELETE FROM Forslag WHERE ForslagsId = @id", new { id = forslagId });
+
+        return Ok(await GetAllForslag());
+    }
+
+    /**
+     * Funksjon for omgjøre bilde til byte.
+     * Vet ikke om denne fungerer helt optimalt
+     */
+    public static byte[] GetBilde(string filepath)
+    {
+        FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+        BinaryReader br = new BinaryReader(fs);
+
+        byte[] bilde = br.ReadBytes((int)fs.Length);
+
+        br.Close();
+        fs.Close();
+
+        return bilde;
+    }
+
+    
 }
