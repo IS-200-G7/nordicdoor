2db7a25d521c6ca31905b000208bc606e451cb8d
Magnus <magnusen@uia.no>
2022-10-25 07:24:55 +0200

    Lagt til controller for testing av div. funksjoner. Til nå kun lesing av JWT token fra header

---
+++ PDSA_System/Server/Controllers/DebugController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDSA_System.Server.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class DebugController : ControllerBase
+{
+    private IConfiguration _configuration;
+
+    public DebugController(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    // login controller
+    [HttpGet("/debug/tokenRead")]
+    [Authorize]
+    public IActionResult Debug()
+    {
+        //Returnerer tom string dersom `fornavn` ikke er definert. Slipper da feilmelding på grunn av nullverdier
+        var fornavn = HttpContext.User.Identities.First().Claims.FirstOrDefault(claim => claim.Type == "fornavn")?.Value;
+
+        return Ok("Hei, " + fornavn);
+    }
+}
