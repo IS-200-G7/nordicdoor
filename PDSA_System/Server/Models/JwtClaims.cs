a32f35f5b5ad1fd58471241e3d6cea78be3d4e0c
Magnus <magnusen@uia.no>
2022-10-25 07:23:11 +0200

    Endret måten JWT secret sendes til `JwtClaims`

--- PDSA_System/Server/Models/JwtClaims.cs
+++ PDSA_System/Server/Models/JwtClaims.cs
@@ -11,18 +11,17 @@ public class JwtClaims
     private string Fornavn { get; set; }
     private string Etternavn { get; set; }
     private string Rolle { get; set; } // Admin, bruker, etc.
-
     private string AnsattNr { get; set; }
-    //private int Exp { get; set; }
+    private string Secret { get; set; }
 
-    public JwtClaims(string epost, string fornavn, string etternavn, string rolle, string ansattNr)
+    public JwtClaims(string epost, string fornavn, string etternavn, string rolle, string ansattNr, string jwtSecret)
     {
         this.Epost = epost;
         this.Fornavn = fornavn;
         this.Etternavn = etternavn;
         this.Rolle = rolle;
         this.AnsattNr = ansattNr;
-        //this.Exp = exp;
+        this.Secret = jwtSecret;
     }
 
     // Generer en JWT token
@@ -40,8 +39,7 @@ public class JwtClaims
         
         // Hente og generere nøkler for autentisering
         // Key er hardcoded kun for utvikling. Denne MÅ endres til å hente fra appsettings.json
-        var key = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes("ED02457B5C41D964DBD2F2A609D63FE1BB7528DBE55E1ABF5B52C249CD735797"));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret ?? "ft2NueB9H8N9p8RpVvWfwWPstApP6gus"));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         // Generer token, issuer og audience kan vi vente med til senere
@@ -64,7 +62,7 @@ public class JwtClaims
 
         if (tokenS == null)
         {
-            return new JwtClaims("", "", "", "", "");
+            return new JwtClaims("", "", "", "", "", null);
         }
 
         var epost = tokenS.Claims.First(claim => claim.Type == "epost").Value;
@@ -76,6 +74,6 @@ public class JwtClaims
         // Exp er ikke nødvendig å hente ut, da den ikke brukes i applikasjonen, bør implementeres senere
         //var exp = tokenS.Claims.First(claim => claim.Type == "exp").Value;
 
-        return new JwtClaims(epost, fornavn, etternavn, rolle, ansattNr);
+        return new JwtClaims(epost, fornavn, etternavn, rolle, ansattNr, null);
     }
 }
