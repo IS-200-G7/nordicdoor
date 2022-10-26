01d0a822634cfb9e4486982516aa2f0472d99d8b
Magnus <magnusen@uia.no>
2022-10-26 12:38:10 +0200

    Standardisering av epostadresser

--- PDSA_System/Server/DataBase/addSeedData.sql
+++ PDSA_System/Server/DataBase/addSeedData.sql
@@ -1,27 +1,27 @@
 -- Bruker
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle)
-VALUES (1, 'Tobias', 'Hansen', 'tob@mail.com',
+VALUES (1, 'Tobias', 'Hansen', 'th@nordic.door',
         'OTOiYdFITlCQGxI+/MGSUO3MGmK46v8LV45DgHFVHNg=:T4sen2e7YnMALZpNRTz5dQJfcg/RZBhnpxVKR2donyg=', 'teamleder');
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle, LederId)
-VALUES (2, 'Siddharth', 'Dushantha', 'satan@sidd.no',
+VALUES (2, 'Siddharth', 'Dushantha', 'sd@nordic.door',
         '02BNAhz1nQOV6Rrdoyalqyug9xh5mJlWmUxgXerpO74=:2k5s/T2rgp0e8vB75+a9fhtUbQFWeWIanwgP/mEqy34=', 'ansatt', 2);
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle)
-VALUES (3, 'Torkel', 'Ivarsøy', 'ivarsøy@example.no',
+VALUES (3, 'Torkel', 'Ivarsøy', 'ti@nordic.door',
         'rcxg5TQUaKneWcQjWwhgZylnkMy46ttyZ9tCEbzKj2w=:tLc7dr65HJ4BeHgQmGTXmnDqsMseTMOMPu/0Sb1Bxy0=', 'teamleder');
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle, LederId)
-VALUES (4, 'Victor', 'Rolf', 'rolf@example.no',
+VALUES (4, 'Victor', 'Rolf', 'vr@nordic.door',
         'cptkiQca0OoHQpIwTOkKs74ybtm6iuYCNw6439BZFgU=:CFdyXnCgT9XnYYAhTUBTKZ/jyfjMg8XuIJIxyO4Gyhc=', 'ansatt', 3);
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle)
-VALUES (5, 'Magnus', 'Menyo', 'menyo@example.no',
+VALUES (5, 'Magnus', 'Menyo', 'mn@nordic.door',
         'njY0hbuir7OBNCcWMDS1th2n3iniWeMEpotApDXDoXo=:D65mEE3hNhijmxLPSrNQcQMhmnV1Auid0hmaS31TqFM=', 'teamleder');
 
 INSERT INTO NordicDoor.Bruker (AnsattNr, Fornavn, Etternavn, Email, PassordHash, Rolle, LederId)
-VALUES (6, 'Eirik', 'Bakkestad', 'bakke@example.no',
+VALUES (6, 'Eirik', 'Bakkestad', 'eb@nordic.door',
         '2l0P/sn5eTgi8i5tJ5fOOXCzJEaU+wVtO+MODE3QlnQ=:H2g9Zs3JYAftr+3vVjV+edFrPO0kpV5OHgwuFToq71A=', 'ansatt', 5);
 
 
@@ -69,24 +69,20 @@ VALUES (1, 2, '"Test forslag"', '"testing testing"', null, "do");
 INSERT INTO NordicDoor.Forslag (ForfatterId, TeamId, Emne, Beskrivelse, Bilde, Status)
 VALUES (5, 2, '"Test forslag"', '"testing testing"', null, "act");
 
-
 -- TeamMedlemskap
 
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (2, 1);
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (2, 2);
-
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (3, 3);
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (3, 4);
-
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (4, 5);
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (4, 6);
-
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
 VALUES (1, 1);
 INSERT INTO NordicDoor.TeamMedlemskap (TeamId, AnsattNr)
@@ -123,4 +119,3 @@ INSERT INTO NordicDoor.ForslagKobling (AnsattNr, ForslagId)
 VALUES (1, 9);
 INSERT INTO NordicDoor.ForslagKobling (AnsattNr, ForslagId)
 VALUES (5, 10);
-
