ab11b08334e5d7c42c711e34be747a968d70952c
EirikBHA <89193729+EirikBHA@users.noreply.github.com>
2022-11-07 15:28:45 +0100

    Funksjon for å legge til bilde i forslag:)

--- PDSA_System/Server/Controllers/ForslagController.cs
+++ PDSA_System/Server/Controllers/ForslagController.cs
@@ -5,6 +5,7 @@ using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Asn1.Pkcs;
 using PDSA_System.Shared.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Data;
 
 namespace PDSA_System.Server.Controllers;
 
@@ -152,5 +153,29 @@ public class ForslagController : Controller
         }
 
         return Ok(); // TODO: Burde kanskje returnert noe?
+    }
+
+    //Metode for å oppdatere Bilde-kolonne i forslag
+    [HttpPost("/api/[controller]/UpdateBilde/")]
+    public async Task<ActionResult<bool>> UpdateBilde(IFormFile imageFile, int forslagId)
+    {
+        var connString = _configuration.GetValue<string>("ConnectionStrings:DefaultConnection");
+        using var conn = new DbHelper(connString).Connection;
+
+        string filePath = Path.GetTempFileName();
+        using (var stream = System.IO.File.Create(filePath))
+        {
+            await imageFile.CopyToAsync(stream);
+        }
+        // Converts image file into byte[]
+        byte[] imageData = await System.IO.File.ReadAllBytesAsync(filePath);
+
+        var parameters = new DynamicParameters();
+        parameters.Add("@Bilde", imageData, DbType.Binary);
+        parameters.Add("@Id", forslagId);
+
+        var res = await conn.ExecuteAsync("UPDATE Forslag SET Bilde = @Bilde WHERE ForslagId = @Id", parameters);
+
+        return Ok(res.Equals(1));
     }
 }
