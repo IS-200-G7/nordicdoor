c404847acab7237b9e883986ccf98ec70d0c681a
torkelhi <torkelhi@uia.no>
2022-11-10 10:25:44 +0100

    Innit commit db-constraints-2
    Endret contraints for Bruker, Team, Forslag.
    Ved trigger DELETE er metoden SET NULL isteden for CASCADE.
    Dette er fordi ved sletting av en bruker eller team, vil alle forslagene relater til det Teamet eller Brukeren som blir slettet også slettet.
    
    1. Dersom en bruker(forfatter) sletter et forslag skal ikke teamet miste sin tilknytning til forslaget og motsatt for team og bruker.
    2. Det blir feil i statistikken på hvor mange forslag som er laget innenfor en tidsperiode og for lag.

--- PDSA_System/Server/DataBase/Create.sql
+++ PDSA_System/Server/DataBase/Create.sql
@@ -3,7 +3,8 @@ USE NordicDoor;
 
 CREATE TABLE IF NOT EXISTS Bruker
 (
-    AnsattNr    INTEGER                 NOT NULL,
+    -- AnsattNr er ikke (NOT NULL) fordi vi bruker ON DELETE SET NULL.
+    AnsattNr    INTEGER                 ,
     Fornavn     VARCHAR(50)             NOT NULL,
     Etternavn   VARCHAR(50)             NOT NULL,
     Email       VARCHAR(254)            NOT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Bruker
     Rolle       VARCHAR(50),
     LederId     INTEGER                 DEFAULT NULL,
     KEY LederId (LederId),
-    FOREIGN KEY (LederId) REFERENCES Bruker (AnsattNr),
+    CONSTRAINT FK_Bruker_Bruker FOREIGN KEY (LederId) REFERENCES Bruker (AnsattNr) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT PK_Bruker PRIMARY KEY (AnsattNr)
 );
 
@@ -21,10 +22,11 @@ DESC Bruker;
 CREATE TABLE IF NOT EXISTS Team
 (
     TeamId      INTEGER NOT NULL AUTO_INCREMENT,
-    TeamLederId INTEGER NOT NULL,
+    TeamLederId INTEGER,
     Navn        VARCHAR(50),
     AvdelingId  INTEGER,
-    CONSTRAINT FK_Team_Bruker FOREIGN KEY (TeamLederId) REFERENCES Bruker (AnsattNr) ON DELETE CASCADE ON UPDATE CASCADE,
+    KEY AvdelingId (AvdelingId),
+    FOREIGN KEY (TeamLederId) REFERENCES Bruker (AnsattNr) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT FK_Team_Team FOREIGN KEY (AvdelingId) REFERENCES Team (TeamId) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT PK_Team PRIMARY KEY (TeamId)
 );
@@ -34,18 +36,18 @@ DESC Team;
 CREATE TABLE IF NOT EXISTS Forslag
 (
     ForslagId     INTEGER                   NOT NULL AUTO_INCREMENT,
-    ForfatterId   INTEGER                   NOT NULL,
+    ForfatterId   INTEGER,
     TeamId        INTEGER                   NOT NULL,
     Emne          VARCHAR(150)              NOT NULL,
     Beskrivelse   VARCHAR(2000)             NOT NULL,
     Bilde         MEDIUMBLOB,
-    Status        VARCHAR(5)                DEFAULT "plan" NOT NULL,
+    Status        VARCHAR(5)                DEFAULT 'plan' NOT NULL,
     Opprettet     DATETIME                  DEFAULT CURRENT_TIMESTAMP,
     SistOppdatert DATETIME                  DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     Frist         DATETIME,
     Kategori      VARCHAR(150),
     CONSTRAINT PK_Forslag PRIMARY KEY (ForslagId),
-    CONSTRAINT FK_Forslag_Bruker FOREIGN KEY (ForfatterId) REFERENCES Bruker (AnsattNr) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_Forslag_Bruker FOREIGN KEY (ForfatterId) REFERENCES Bruker (AnsattNr) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT FK_Forlsag_Team FOREIGN KEY (TeamId) REFERENCES Team (TeamId) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
